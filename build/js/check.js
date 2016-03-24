9deaf047e63870bfb893ff8d6c9f575dd965d228
N0rr <xt12pabeh@mail.ru>
2016-03-24 19:44:40 +0300

    upd 3

--- build/js/check.js
+++ build/js/check.js
@@ -1,24 +1,26 @@
 
-function getMessage(a, b) {
-    var a = [11,11];
-    var b = [11,12];   
+function getMessage(a, b) {   
     var sum = 0;
-    var length = 0;
+    var length = 0;   
+
     if(a === true){        
         return 'Я попал в ' + b;
     }
     else if(a === false){
         return 'Я никуда не попал';
-    } 
+    }
+    
     else if(a  instanceof Number) {
         return 'Я прыгнул на ' + a * 100 + ' сантиметров'; 
     }
+    
     else if((a instanceof Array) && (b instanceof Array === false)) {           
         for(i=0; i < a.length; i++) {
             sum += a[i];             
         }
         return 'Я прошел ' + sum + ' шагов';                
     } 
+    
     else if((a instanceof Array) && (b instanceof Array)) {
         for(i=0; i < a.length; i++) {
             sum += a[i];             
