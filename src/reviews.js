af470d071055cfd509d8792d1fc8f0cca5724d09
N0rr <xt12pabeh@mail.ru>
2016-04-08 18:47:06 +0300

    timeout

--- src/reviews.js
+++ src/reviews.js
@@ -6,7 +6,9 @@
   var templateReview = document.querySelector('#review-template');
   var imgTimeOut = 10000;
   var reviewClone;
+
   reviewFilter.classList.add('invisible');
+
   if ('content' in templateReview) {
     reviewClone = templateReview.content.querySelector('.review');
   } else {
@@ -15,23 +17,29 @@
 
   var getReview = function(data, container) {
     var clone = reviewClone.cloneNode(true);
+
     reviewFilter.classList.remove('invisible');
     clone.querySelector('.review-text').textContent = data.description;
     container.appendChild(clone);
+
     var PhotoAvatar = new Image();
     var avatarLoadTimeout;
+
     PhotoAvatar.onload = function(evt) {
       clearTimeout(avatarLoadTimeout);
       clone.querySelector('.review-author').src = evt.target.src;
       clone.querySelector('.review-author').width = 124;
       clone.querySelector('.review-author').height = 124;
     };
+
     PhotoAvatar.onerror = function() {
       clone.classList.add('review-load-failure');
     };
+
     PhotoAvatar.src = data.author.picture;
     avatarLoadTimeout = setTimeout(function() {
       PhotoAvatar.src = '';
+      clone.classList.add('review-load-failure');
     }, imgTimeOut);
     return clone;
   };
