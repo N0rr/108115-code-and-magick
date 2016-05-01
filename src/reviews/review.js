8105d90bb9d2bcae261b1d8b8d065858f6b50f9f
N0rr <xt12pabeh@mail.ru>
2016-05-02 00:38:12 +0300

    prototype

--- src/reviews/review.js
+++ src/reviews/review.js
@@ -16,38 +16,34 @@
   var Review = function(data, container) {
     this.data = data;
 
-    this.getReview = function() {
-
-      var clone = reviewClone.cloneNode(true);
-      var photoAvatar = clone.querySelector('.review-author');
-      var reviewText = clone.querySelector('.review-text');
-
-      reviewText.textContent = data.description;
-
-      utilities.createNewImage(data.author.picture, function(error) {
-        if (error) {
-          clone.classList.add('review-load-failure');
-        } else {
-          photoAvatar.src = data.author.picture;
-          photoAvatar.width = 124;
-          photoAvatar.height = 124;
-        }
-      });
-
-      container.appendChild(clone);
-
-      return clone;
-    };
-
     this.element = this.getReview(this.data, container);
 
-    this.remove = function() {
-      this.element.removeEventListener('click', this.onClickReviewQuiz);
-      this.element.parentNode.removeChild(this.element);
-    };
-
     this.element.addEventListener('click', this.onClickReviewQuiz);
+    this.onClickReviewQuiz = this.onClickReviewQuiz.bind(this);
 
+  };
+
+  Review.prototype.getReview = function(_data, _container) {
+
+    var clone = reviewClone.cloneNode(true);
+    var photoAvatar = clone.querySelector('.review-author');
+    var reviewText = clone.querySelector('.review-text');
+
+    reviewText.textContent = _data.description;
+
+    utilities.createNewImage(_data.author.picture, function(error) {
+      if (error) {
+        clone.classList.add('review-load-failure');
+      } else {
+        photoAvatar.src = _data.author.picture;
+        photoAvatar.width = 124;
+        photoAvatar.height = 124;
+      }
+    });
+
+    _container.appendChild(clone);
+
+    return clone;
   };
 
   Review.prototype.onClickReviewQuiz = function(evt) {
@@ -55,6 +51,11 @@
       evt.preventDefault();
       evt.target.classList.add('review-quiz-answer-active');
     }
+  };
+
+  Review.prototype.remove = function() {
+    this.element.removeEventListener('click', this.onClickReviewQuiz);
+    this.element.parentNode.removeChild(this.element);
   };
 
   module.exports = Review;
