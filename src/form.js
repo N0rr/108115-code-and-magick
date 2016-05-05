f61db04f49a9b4141d3cc3c0d1060aeb2cde2438
N0rr <xt12pabeh@mail.ru>
2016-05-05 13:05:12 +0300

    fix

--- src/form.js
+++ src/form.js
@@ -1,132 +1,130 @@
 'use strict';
 
-(function() {
-  var browserCookies = require('browser-cookies');
-  var formContainer = document.querySelector('.overlay-container');
-  var formOpenButton = document.querySelector('.reviews-controls-new');
-  var formCloseButton = document.querySelector('.review-form-close');
-  var formName = document.querySelector('#review-name');
-  var formText = document.querySelector('#review-text');
-  var formCheckbox = document.getElementsByName('review-mark');
-  var labelCheck = document.querySelector('.review-form-group');
-  var tipName = document.querySelector('.review-fields-name');
-  var tipText = document.querySelector('.review-fields-text');
-  var formButton = document.querySelector('.review-submit');
-  var formTip = document.querySelector('.review-fields');
-  var formSubmit = document.querySelector('.overlay');
-  var checkforCookie;
+var browserCookies = require('browser-cookies');
+var formContainer = document.querySelector('.overlay-container');
+var formOpenButton = document.querySelector('.reviews-controls-new');
+var formCloseButton = document.querySelector('.review-form-close');
+var formName = document.querySelector('#review-name');
+var formText = document.querySelector('#review-text');
+var formCheckbox = document.getElementsByName('review-mark');
+var labelCheck = document.querySelector('.review-form-group');
+var tipName = document.querySelector('.review-fields-name');
+var tipText = document.querySelector('.review-fields-text');
+var formButton = document.querySelector('.review-submit');
+var formTip = document.querySelector('.review-fields');
+var formSubmit = document.querySelector('.overlay');
+var checkforCookie;
 
-  var cookieStart = function() {
-    formName.value = browserCookies.get('formName') || '';
-    checkforCookie = browserCookies.get('checkforCookie') || 3;
-    document.querySelector('#review-mark-' + checkforCookie).checked = true;
-  };
+var cookieStart = function() {
+  formName.value = browserCookies.get('formName') || '';
+  checkforCookie = browserCookies.get('checkforCookie') || 3;
+  document.querySelector('#review-mark-' + checkforCookie).checked = true;
+};
 
-  cookieStart();
+cookieStart();
 
-  formButton.disabled = true;
-  formName.required = true;
+formButton.disabled = true;
+formName.required = true;
 
-  function checked(checkbox, formtext, button, formname, tip, tiptext) {
-    var check;
+function checked(checkbox, formtext, button, formname, tip, tiptext) {
+  var check;
 
-    for (var i = 0; i < checkbox.length; i++) {
-      if (checkbox[i].checked && checkbox[i].value >= 3) {
-        check = true;
-      } else if (checkbox[i].checked && checkbox[i].value < 3) {
-        check = false;
-      }
-    }
-    if (!check) {
-      formText.required = true;
-      button.disabled = true;
-      if (!formtext.validity.valid) {
-        tiptext.classList.remove('invisible');
-        tip.classList.remove('invisible');
-      }
-    } else {
-      formText.required = false;
-      tiptext.classList.add('invisible');
-      if (formname.validity.valid && formtext.validity.valid) {
-        button.disabled = false;
-        tiptext.classList.add('invisible');
-        tip.classList.add('invisible');
-      }
+  for (var i = 0; i < checkbox.length; i++) {
+    if (checkbox[i].checked && checkbox[i].value >= 3) {
+      check = true;
+    } else if (checkbox[i].checked && checkbox[i].value < 3) {
+      check = false;
     }
   }
-
-  function checkValid(nameform, tipform) {
-    if (nameform.validity.valid) {
-      tipform.classList.add('invisible');
-    } else {
-      tipform.classList.remove('invisible');
+  if (!check) {
+    formText.required = true;
+    button.disabled = true;
+    if (!formtext.validity.valid) {
+      tiptext.classList.remove('invisible');
+      tip.classList.remove('invisible');
     }
-  }
-
-  function tipClose(formname, button, formtip, formtext) {
+  } else {
+    formText.required = false;
+    tiptext.classList.add('invisible');
     if (formname.validity.valid && formtext.validity.valid) {
       button.disabled = false;
-      formTip.classList.add('invisible');
-    } else {
-      button.disabled = true;
-      formTip.classList.remove('invisible');
+      tiptext.classList.add('invisible');
+      tip.classList.add('invisible');
     }
   }
+}
 
-  formName.oninput = function() {
-    checkValid(formName, tipName);
-  };
+function checkValid(nameform, tipform) {
+  if (nameform.validity.valid) {
+    tipform.classList.add('invisible');
+  } else {
+    tipform.classList.remove('invisible');
+  }
+}
 
-  formName.onfocus = function() {
-    checkValid(formName, tipName);
-    checked(formCheckbox, formText, formButton, formName, formTip, tipText);
-  };
+function tipClose(formname, button, formtip, formtext) {
+  if (formname.validity.valid && formtext.validity.valid) {
+    button.disabled = false;
+    formTip.classList.add('invisible');
+  } else {
+    button.disabled = true;
+    formTip.classList.remove('invisible');
+  }
+}
 
-  formText.oninput = function() {
-    checkValid(formText, tipText);
-  };
+formName.oninput = function() {
+  checkValid(formName, tipName);
+};
 
-  formContainer.oninput = function() {
-    tipClose(formName, formButton, formTip, formText);
-  };
+formName.onfocus = function() {
+  checkValid(formName, tipName);
+  checked(formCheckbox, formText, formButton, formName, formTip, tipText);
+};
 
-  labelCheck.addEventListener('click', function() {
-    checked(formCheckbox, formText, formButton, formName, formTip, tipText);
+formText.oninput = function() {
+  checkValid(formText, tipText);
+};
+
+formContainer.oninput = function() {
+  tipClose(formName, formButton, formTip, formText);
+};
+
+labelCheck.addEventListener('click', function() {
+  checked(formCheckbox, formText, formButton, formName, formTip, tipText);
+});
+
+formOpenButton.onclick = function(evt) {
+  evt.preventDefault();
+  formContainer.classList.remove('invisible');
+  formName.focus();
+};
+
+formCloseButton.onclick = function(evt) {
+  evt.preventDefault();
+  formContainer.classList.add('invisible');
+};
+
+formSubmit.onsubmit = function(evt) {
+  evt.preventDefault();
+  var presentDate = new Date();
+  var birthday = new Date(presentDate.getFullYear(), 1, 3);
+  var oneYear = 365 * 24 * 60 * 60 * 1000;
+  var oneDay = 24 * 60 * 60 * 1000;
+  var timeAfterBirthday = Math.floor((presentDate.valueOf() - birthday.valueOf()) / oneDay);
+  var cookieLife = new Date((timeAfterBirthday.valueOf() * oneDay) + presentDate.valueOf());
+
+  checkforCookie = document.querySelector('input[name=review-mark]:checked');
+  if (birthday.valueOf() > presentDate.valueOf()) {
+    cookieLife = new Date(cookieLife.valueOf() + oneYear);
+  }
+
+  browserCookies.set('formName', formName.value, {
+    expires: cookieLife
   });
 
-  formOpenButton.onclick = function(evt) {
-    evt.preventDefault();
-    formContainer.classList.remove('invisible');
-    formName.focus();
-  };
+  browserCookies.set('checkforCookie', checkforCookie.value, {
+    expires: cookieLife
+  });
 
-  formCloseButton.onclick = function(evt) {
-    evt.preventDefault();
-    formContainer.classList.add('invisible');
-  };
-
-  formSubmit.onsubmit = function(evt) {
-    evt.preventDefault();
-    var presentDate = new Date();
-    var birthday = new Date(presentDate.getFullYear(), 1, 3);
-    var oneYear = 365 * 24 * 60 * 60 * 1000;
-    var oneDay = 24 * 60 * 60 * 1000;
-    var timeAfterBirthday = Math.floor((presentDate.valueOf() - birthday.valueOf()) / oneDay);
-    var cookieLife = new Date((timeAfterBirthday.valueOf() * oneDay) + presentDate.valueOf());
-
-    checkforCookie = document.querySelector('input[name=review-mark]:checked');
-    if (birthday.valueOf() > presentDate.valueOf()) {
-      cookieLife = new Date(cookieLife.valueOf() + oneYear);
-    }
-
-    browserCookies.set('formName', formName.value, {
-      expires: cookieLife
-    });
-
-    browserCookies.set('checkforCookie', checkforCookie.value, {
-      expires: cookieLife
-    });
-
-    formSubmit.submit();
-  };
-})();
+  formSubmit.submit();
+};
